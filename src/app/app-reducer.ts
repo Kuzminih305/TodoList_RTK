fc0a46933c571b1b2bffabf70d678e08008d6714
Kuzminih305 <Kuzminih187@gmail.com>
2023-06-23 20:49:34 +0300

    RTK

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,61 @@
+import {Dispatch} from 'redux'
+import {authAPI} from '../api/todolists-api'
+import {authActions} from "features/Login/auth-reducer";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+
+// const initialState: InitialStateType = {
+//     status: 'idle',
+//     error: null,
+//     isInitialized: false
+// }
+
+const slice = createSlice({
+    name: 'app',
+    initialState: {
+        status: 'idle' as RequestStatusType,
+        error: null as null | string,
+        isInitialized: false
+    },
+    reducers: {
+        setStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+            state.status = action.payload.status
+        },
+        setError: (state, action: PayloadAction<{ error: null | string }>) => {
+            state.error = action.payload.error
+        },
+        setIsInitialized: (state, action: PayloadAction<{isInitialized: boolean}>) => {
+            state.isInitialized = action.payload.isInitialized
+        }
+    },
+
+})
+export const appReducer = slice.reducer
+export const appActions = slice.actions
+
+
+
+// export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+//     switch (action.type) {
+//         case 'APP/SET-STATUS':
+//             return {...state, status: action.status}
+//         case 'APP/SET-ERROR':
+//             return {...state, error: action.error}
+//         case 'APP/SET-IS-INITIALIED':
+//             return {...state, isInitialized: action.value}
+//         default:
+//             return {...state}
+//     }
+// }
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me().then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(authActions.setIsLoggedIn({isLoggedIn: true}));
+        } else {
+        }
+        dispatch(appActions.setIsInitialized({isInitialized: true}))
+    })
+}
